fbfbca932693cf52c229da945e7a3a1670dae7bb
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-10-04 13:05:55 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/GabrielMazzieiro/gopportunities/config"
 	"github.com/GabrielMazzieiro/gopportunities/router"
 )
 
 func main() {
+
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
